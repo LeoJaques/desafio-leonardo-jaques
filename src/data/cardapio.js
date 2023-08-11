2daaaa56bb65e4bdf0bccc146ed21f561e95b0f0
Leonardo Jaques <103287079+LeoJaques@users.noreply.github.com>
2023-08-11 11:37:16 -0400

    exclusão de atributo não utilizado

--- src/data/cardapio.js
+++ src/data/cardapio.js
@@ -1,41 +1,33 @@
 const cardapio = {
     cafe: {
-        codigo: "cafe",
         descricao: "Café",
         valor: 300,
     },
     chantily: {
-        codigo: "chantily",
         descricao: "Chantily (extra do Café)",
         valor: 150,
     },
     suco: {
-        codigo: "suco",
         descricao: "Suco Natural",
         valor: 620,
     },
     sanduiche: {
-        codigo: "sanduiche",
         descricao: "Sanduíche",
         valor: 650,
     },
     queijo: {
-        codigo: "queijo",
         descricao: "Queijo (extra do Sanduíche)",
         valor: 200,
     },
     salgado: {
-        codigo: "salgado",
         descricao: "Salgado",
         valor: 725,
     },
     combo1: {
-        codigo: "combo1",
         descricao: "1 Suco e 1 Sanduíche",
         valor: 950,
     },
     combo2: {
-        codigo: "combo2",
         descricao: "1 Café e 1 Sanduíche",
         valor: 750,
     },
