99f7178d4df89ce772f03781a6d9ead23760e30d
Leonardo Jaques <leonardo.yan19@outlook.com>
2023-08-11 23:19:05 -0300

    desafio finalizado

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,42 +1,55 @@
-import { validarMetodoPagamento } from './controllers/metodosPagamento'
+import { aplicarDescontoOuAcrescimo, validarMetodoPagamento } from './controllers/metodosPagamento'
 import { cardapio } from './data/cardapio'
 
 class CaixaDaLanchonete {
+  calcularValorDaCompra(metodoDePagamento, itens) {
+    if (!validarMetodoPagamento(metodoDePagamento))
+      return 'Forma de pagamento inválida!'
 
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        if (!validarMetodoPagamento(metodoDePagamento)) {
-            return 'Forma de pagamento inválida!'
+    if (itens.length === 0) return 'Não há itens no carrinho de compra!'
+
+    const pedido = []
+    let valorTotal = 0
+    
+    for (let item of itens) {
+      let [produto, quant] = item.split(',')
+
+      if (quant === '0') return 'Quantidade inválida!'
+      
+      if (!cardapio[produto]) return 'Item inválido!'
+      
+      const pedidoItem = {
+          produto,
+          descricao: cardapio[produto].descricao,
+          quant
         }
-
-        const pedido = []
         
+        pedido.push(pedidoItem)
 
-        if (itens.length === 0) {
-            return 'Não há itens no carrinho de compra!'
+        valorTotal += cardapio[produto].valor * quant
+    }
+    
+
+
+    for (let produto of pedido) {
+      if (cardapio[produto.produto].descricao.includes('extra')) {
+        const descricao = cardapio[produto.produto].descricao
+        const nomeProdutoPrincipal = descricao
+          .split(' (extra do ')[1]
+          .replace(')', '')
+
+        if (!pedido.some(item => item.descricao === nomeProdutoPrincipal)) {
+          return 'Item extra não pode ser pedido sem o principal'
         }
-
-        for (let item of itens) {
-            let [produto, quant] = item.split(',')
-
-            if (quant === '0') return 'Quantidade inválida!'
-
-            if (!cardapio[produto]) return 'Item inválido!'
-
-            const pedidoItem = {
-                produto,
-                quant
-            }
-            
-
-            pedido.push(pedidoItem)
-        }
-
-
-
-
-        return "";
+      }
     }
 
+    const valorTotalComDesconto = aplicarDescontoOuAcrescimo(valorTotal, metodoDePagamento) / 100
+
+
+
+    return `R$ ${valorTotalComDesconto.toFixed(2).toString().replace('.', ',')}`
+  }
 }
 
-export { CaixaDaLanchonete };
+export { CaixaDaLanchonete }
